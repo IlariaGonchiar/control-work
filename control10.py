63cb9677433181d25c4bf243707a95f3831ef8d8
IlariaGonchiar <125513828+IlariaGonchiar@users.noreply.github.com>
2023-02-27 00:21:51 +0200

    Add files via upload

---
+++ control10.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    return a+b
+print(sum(2, 6))
+def difference(a, b):
+    return a-b
+print(difference(7, 4))
+def multiplication(a, b):
+    return a*b
+print(multiplication(3, 5))
+def multiplication(a, b):
+    return a*b
+print(3, 7)
+def D(a, b):
+    return a**2-b**2
+print(D(2, 3))
